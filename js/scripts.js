c8d2928eb4b9c8ef534757720f3c151eecf5bd70
jacksonlevine <jack@netbug.us>
2023-01-11 13:16:05 -0800

    add script to generate an html element for every animal in a ul list, with phony text

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,15 @@ onload = function() {
     const selection = document.getElementById("exhibit").value;
     let divquery = document.querySelector("div.show"); //look for div .show in the document
     if(divquery != null) {
-      divquery.setAttribute("class", "hidden") //hide any showing ones
+      document.body.removeChild(divquery); //hide any showing ones
     }
 
-    let showquery = document.querySelector("div#" + selection);
+    let showquery = document.querySelector("ul.animals>li#" + selection);
     if(showquery != null) {
-      showquery.setAttribute("class", "show")
+      var slide = document.createElement("div");
+      slide.setAttribute("class", "show")
+      slide.innerHTML = "<img src=\"img/"+selection+".jpg\">" + "<p> The " + selection + " is lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora minus maiores quibusdam, similique quod, expedita provident voluptate reprehenderit blanditiis nam sunt animi quam, adipisci nostrum rerum illo enim debitis corporis</p>";
+      document.body.appendChild(slide);
     } else {
       window.alert("There is no exhibit for "+ selection);
     }
