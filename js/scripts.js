62fff3350a40d12e81b9e1d0504b12315ae9f38b
jacksonlevine <jack@netbug.us>
2023-01-11 19:00:14 -0800

    improved

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,12 @@ onload = function() {
       var slide = document.createElement("div");
       slide.setAttribute("class", "show")
       slide.innerHTML = "<img src=\"img/"+selection+".jpg\">" + "<p> The " + selection + " is lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora minus maiores quibusdam, similique quod, expedita provident voluptate reprehenderit blanditiis nam sunt animi quam, adipisci nostrum rerum illo enim debitis corporis</p>";
+      let rvalue = Math.random()*99;
+      let gvalue = Math.random()*99;
+      let bvalue = Math.random()*99;
+      slide.style.backgroundColor = "rgb(" + rvalue + "," + gvalue + "," + bvalue + ")";
+      slide.style.color = "rgb(" + (255-rvalue) + "," + (255-gvalue) + ",255)"
+      slide.style.borderColor = "rgb(" + (255-rvalue) + "," + (255-gvalue) + ",255)"
       document.body.appendChild(slide);
     } else {
       window.alert("There is no exhibit for "+ selection);
